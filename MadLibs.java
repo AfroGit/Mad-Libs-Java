bbad30bb968df6954502c742f465e0b9ee65b46a
AfroGit <94635472+AfroGit@users.noreply.github.com>
2023-09-01 19:33:09 +0000

    Initial commit

---
+++ MadLibs.java
@@ -0,0 +1,20 @@
+/*
+* # Author; Maurice Mutua
+* # Goal; To learn the basics of using plus importing a    scanner,concatnation,common uses of operators andprimitive data types.
+* # Test Automation University collabo
+* # Date; 9/01/2023
+*/
+
+import java.util.Scanner;
+
+public class MadLibs{
+
+  public static void main(String[] args){
+
+    
+
+    System.out.println("Hello world!");
+    
+  }
+  
+}
